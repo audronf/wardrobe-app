3cb964c4846cefd8e5049b245e3f9ba488d1b7a9
Federico <33864194+audronf@users.noreply.github.com>
2022-10-10 13:16:13 -0300

    Updated icons for every clothes category (#3)

--- lib/add_clothes.dart
+++ lib/add_clothes.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-
 import 'components/clothes_category.dart';
-
 class AddClothes extends StatelessWidget {
   const AddClothes({super.key});
 
@@ -12,23 +10,23 @@ class AddClothes extends StatelessWidget {
           title: const Text('Add new clothes'),
         ),
         body: Container(
-            padding: EdgeInsets.all(8),
-            child: GridView.count(crossAxisCount: 3, mainAxisSpacing: 8, crossAxisSpacing: 8, children: <Widget>[
-              ClothesCategory(image: 'assets/ic_tshirt.png', text: 'Jacket'),
-              ClothesCategory(image: 'assets/ic_tshirt.png', text: 'Hoodie'),
+            padding: const EdgeInsets.all(8),
+            child: GridView.count(crossAxisCount: 3, mainAxisSpacing: 8, crossAxisSpacing: 8, children: const <Widget>[
+              ClothesCategory(image: 'assets/ic_jacket.png', text: 'Jacket'),
+              ClothesCategory(image: 'assets/ic_hoodie.png', text: 'Hoodie'),
+              ClothesCategory(image: 'assets/ic_coat.png', text: 'Coat'),
               ClothesCategory(image: 'assets/ic_tshirt.png', text: 'T-Shirt'),
-              ClothesCategory(image: 'assets/ic_tshirt.png', text: 'Shirt'),
-              ClothesCategory(image: 'assets/ic_tshirt.png', text: 'Tank top'),
-              ClothesCategory(image: 'assets/ic_tshirt.png', text: 'Shorts'),
-              ClothesCategory(image: 'assets/ic_tshirt.png', text: 'Pants'),
-              ClothesCategory(image: 'assets/ic_tshirt.png', text: 'Dress'),
-              ClothesCategory(image: 'assets/ic_tshirt.png', text: 'Skirt'),
-              ClothesCategory(image: 'assets/ic_tshirt.png', text: 'Underwear'),
-              ClothesCategory(image: 'assets/ic_tshirt.png', text: 'Shoes'),
-              ClothesCategory(image: 'assets/ic_tshirt.png', text: 'Socks'),
-              ClothesCategory(image: 'assets/ic_tshirt.png', text: 'Scarf'),
-              ClothesCategory(image: 'assets/ic_tshirt.png', text: 'Coat'),
-              ClothesCategory(image: 'assets/ic_tshirt.png', text: 'Other'),
+              ClothesCategory(image: 'assets/ic_shirt.png', text: 'Shirt'),
+              ClothesCategory(image: 'assets/ic_tank_top.png', text: 'Tank top'),
+              ClothesCategory(image: 'assets/ic_shorts.png', text: 'Shorts'),
+              ClothesCategory(image: 'assets/ic_pants.png', text: 'Pants'),
+              ClothesCategory(image: 'assets/ic_dress.png', text: 'Dress'),
+              ClothesCategory(image: 'assets/ic_skirt.png', text: 'Skirt'),
+              ClothesCategory(image: 'assets/ic_underwear.png', text: 'Underwear'),
+              ClothesCategory(image: 'assets/ic_footwear.png', text: 'Footwear'),
+              ClothesCategory(image: 'assets/ic_socks.png', text: 'Socks'),
+              ClothesCategory(image: 'assets/ic_scarf.png', text: 'Scarf'),
+              ClothesCategory(image: 'assets/ic_other.png', text: 'Other'),
             ])));
   }
 }
