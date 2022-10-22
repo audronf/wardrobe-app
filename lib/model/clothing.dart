d0f380267b4b070f5f6cf11b4404fe2f590ee973
Federico <33864194+audronf@users.noreply.github.com>
2022-10-22 12:05:45 -0300

    Added sqflite dependency and basic clothing model (#4)

---
+++ lib/model/clothing.dart
@@ -0,0 +1,15 @@
+class Clothing {
+  final int id;
+  final String type;
+  final String size;
+  final String description;
+  final String color;
+
+  const Clothing({
+    required this.id,
+    required this.type,
+    required this.size,
+    required this.description,
+    required this.color
+  });
+}
